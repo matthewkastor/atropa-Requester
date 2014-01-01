791d3fcb3126b8afa96afaea0c7f4bc6bdb87642
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 13:36:38 -0500

    updated docs

--- docs/vsdoc/OpenLayersAll.js
+++ docs/vsdoc/OpenLayersAll.js
@@ -26,16 +26,109 @@
 
     window.atropa = {
         /// <summary></summary>
-        /// <field name="ArgsInfo" type="">Required module, the docs for it are in the &lt;code&gt;
-        ///  atropa-ArgsInfo/docs&lt;/code&gt; directory where this module 
+        /// <field name="data" type="">Container for gobal data related to the classes and functions.</field>
+        /// <field name="inquire" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-inquire/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="arrays" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-arrays/docs&lt;/code&gt; directory where this module 
+        ///  is located.</field>
+        /// <field name="customErrors" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-customErrors/docs&lt;/code&gt; directory where this module 
         ///  is located.</field>
         /// <returns type="atropa"/>
                 
+        supportCheck: function(className, errorMessage) {
+            /// <summary>Checks whether this class has been marked as unsupported and throws an 
+            ///  error if it has.</summary>
+            /// <param name="className" type="String">The name of the class.</param>
+            /// <param name="errorMessage" type="String">Optional. A custom error message. Defaults to
+            ///  atropa.data[className].error</param>
+        }, 
+        
+        requires: function(className, requirementFn, errorMessage) {
+            /// <summary>Pushes a requirement check into atropa.data.requirements. The test
+            ///  tests whether the class is supported in this environment. Sets
+            ///  atropa.data[className]&apos;s support to unsupported and error to errorMessage
+            ///  if the requirementFn returns false. The requirement checks will all be run
+            ///  after the library has loaded.</summary>
+            /// <param name="className" type="String">The name of the class.</param>
+            /// <param name="requirementFn" type="Function">A function to test whether or not the class
+            ///  is supported in this environment. If supported, returns true otherwise
+            ///  return false.</param>
+            /// <param name="errorMessage" type="String">The error message to use when this class or its
+            ///  methods are called in unsupported environments. Defaults to:
+            ///  &apos;The atropa.&apos; + className + &apos; class is unsupported in this environment.&apos;;</param>
+        }
+        
     };
 
     var $x = window.atropa;
     $x.__namespace = "true";
     $x.__typeName = "atropa";
+})(this);
+
+  
+
+  
+  
+/* vsdoc for atropa.ArgsInfo */
+
+(function (window) {
+    window.atropa = window.atropa || {};
+
+    window.atropa.ArgsInfo = function(){
+        /// <summary></summary>
+        /// <returns type="atropa.ArgsInfo"/>
+    };
+
+    var $x = window.atropa.ArgsInfo;
+    $x.prototype = {
+                
+        setExpectedArgTypes: function(typesObj) {
+            /// <summary>Sets the expected argument types.</summary>
+            /// <param name="typesObj" type="Expected Arg Types">An object containing information
+            ///  about the types of arguments you expect. Specifically, the object should
+            ///  look like the example.</param>
+        }, 
+        
+        getArgTypes: function(args) {
+            /// <summary>Gets the types of arguments.</summary>
+            /// <param name="args" type="arguments">An arguments object, or anything you want to
+            /// check the type of.</param>
+            /// <returns type="Array">Returns an array of the types of arguments passed in.</returns>
+        }, 
+        
+        checkArgTypes: function(args) {
+            /// <summary>Checks the given arguments object against the expected
+            /// arguments types.</summary>
+            /// <param name="args" type="arguments">An arguments object</param>
+            /// <returns type="String">The user assigned key which matches the
+            /// arguments supplied, or throws an error.</returns>
+        }
+        
+    };
+
+    $x.__class = "true";
+    $x.__typeName = "atropa.ArgsInfo";
+})(this);
+
+
+  
+/* vsdoc for atropa.data */
+
+(function (window) {
+    window.atropa = window.atropa || {};
+
+    window.atropa.data = {
+        /// <summary></summary>
+        /// <returns type="atropa.data"/>
+                
+    };
+
+    var $x = window.atropa.data;
+    $x.__namespace = "true";
+    $x.__typeName = "atropa.data";
 })(this);
 
   
--- docs/vsdoc/OpenLayersReferences.js
+++ docs/vsdoc/OpenLayersReferences.js
@@ -1,4 +1,6 @@
 
 /// <reference path="atropa-vsdoc.js" />
+/// <reference path="atropa.ArgsInfo-vsdoc.js" />
+/// <reference path="atropa.data-vsdoc.js" />
 /// <reference path="atropa.Requester-vsdoc.js" />
 
