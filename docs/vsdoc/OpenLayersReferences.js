c929dd9d5a68e68de0da73d288abe086ed4c695a
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 17:45:28 -0500

    updated docs

---
+++ docs/vsdoc/OpenLayersReferences.js
@@ -0,0 +1,6 @@
+
+/// <reference path="atropa-vsdoc.js" />
+/// <reference path="atropa.ArgsInfo-vsdoc.js" />
+/// <reference path="atropa.data-vsdoc.js" />
+/// <reference path="atropa.Requester-vsdoc.js" />
+
